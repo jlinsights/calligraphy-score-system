5a393f7baaa9ab8f827a802678348e0ae736487e
Jaehong <jhlim725@gmail.com>
2025-04-13 23:21:54 +0900

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,15 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: './',
+  build: {
+    outDir: 'docs',
+    assetsDir: 'assets',
+  },
   server: {
     host: "::",
-    port: 8080,
+    port: 8082,
   },
   plugins: [
     react(),
